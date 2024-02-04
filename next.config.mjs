53ef19ca53639cd0c91a32e8eefe24bbe10a275f
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-02-04 14:01:08 -0500

    Setup Local Data Base

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            'uploadthing.com',
+            'utfs.io',
+            'img.clerk.com',
+            'subdomain',
+            'file.stripe.com',
+        ],
+    },
+    reactStrictMode: false,
+};
 
 export default nextConfig;
